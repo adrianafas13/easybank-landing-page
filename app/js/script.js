5a0524edfcd02fd854255b4d9d158d7cfc3aa4ed
Adriana <adrianafas13@hotmail.com>
2021-07-03 23:00:51 -0400

    mobile menu part 1

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,12 @@
-console.log('hello future!');
+const btnHamburguer = document.querySelector('#btnHamburguer');
+
+btnHamburguer.addEventListener('click', function(){
+    console.log('click hamburguer');
+
+    if(btnHamburguer.classList.contains('open')){
+        btnHamburguer.classList.remove('open');
+    }
+    else{
+        btnHamburguer.classList.add('open');
+    }
+});
