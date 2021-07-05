a75b6723dcb9fe22406e21a1f228dcd9f4867941
Adriana <adrianafas13@hotmail.com>
2021-07-04 22:21:41 -0400

    mobile menu part 3

--- app/js/script.js
+++ app/js/script.js
@@ -1,13 +1,18 @@
 const btnHamburguer = document.querySelector('#btnHamburguer');
 const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
 
 btnHamburguer.addEventListener('click', function(){
     console.log('click hamburguer');
 
-    if(header.classList.contains('open')){
+    if(header.classList.contains('open')){//close mobile menu
         header.classList.remove('open');
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
     }
-    else{
+    else{ //open mobile menu
         header.classList.add('open');
+        overlay.classList.remove('fade-out');
+        overlay.classList.add('fade-in');
     }
 });
